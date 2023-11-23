b62f856fe387b2ccf936115f82de7df975874d3c
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2023-11-23 14:18:17 -0400

    challenge finish 2023-11-23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,28 +23,28 @@ const SummariesArr: SummariesProps[] = [
 export default function Home() {
   return (
     <main className="bg-[#f1f1f1] flex w-full h-screen justify-center items-center">
-      <div className="flex flex-col w-3/4 h-[90%] sm:w-2/4 md:flex-row md:w-[70%] md:h-2/4 xl:w-[40%] xl:h-2/3 bg-white rounded-3xl">
-        <div className="bg-gradient-to-b h-[50%] md:h-full md:w-2/4 xl:w-2/4 from-indigo-500 to-indigo-800 text-white rounded-3xl text-center items-center flex flex-col">
-          <p className="text-sm md:text-base md:my-7 xl:my-9 xl:text-xl opacity-70 my-5">
+      <div className="flex flex-col w-3/4 h-[90%] sm:w-2/4 md:flex-row md:w-[70%] md:h-2/4 bg-white rounded-3xl">
+        <div className="bg-gradient-to-b h-[50%] md:h-full md:w-2/4 xl:w-1/4 from-indigo-500 to-indigo-800 text-white rounded-3xl text-center items-center flex flex-col">
+          <p className="text-sm md:text-base md:my-7 opacity-70 my-10">
             Your Result
           </p>
 
-          <div className="w-[40%] h-[38%] md:w-2/4 xl:w-[65%] xl:h-[40%] bg-gradient-to-b from-[#281979] flex flex-col rounded-full justify-center items-center">
-            <p className="text-5xl md:text-7xl xl:text-7xl">76</p>
+          <div className="w-[40%] h-[38%] md:w-2/4 xl:w-2/3 bg-gradient-to-b from-[#281979] flex flex-col rounded-full justify-center items-center">
+            <p className="text-5xl md:text-7xl">76</p>
             <p className="text-xs md:text-base opacity-50">of 100</p>
           </div>
 
-          <p className="text-xl mt-5 mb-2 md:text-2xl md:mb-3 md:mt-6 xl:text-2xl xl:mt-8">
+          <p className="text-xl mt-5 mb-2 md:text-2xl md:mb-3 md:mt-6">
             Great
           </p>
           <div className="w-3/4 mb-5">
-            <p className="text-xs opacity-50 font-black md:text-sm xl:text-base">
+            <p className="text-xs opacity-50 font-black md:text-sm">
               You scored higher than 65% of the people who have taken these tests.
             </p>
           </div>
         </div>
 
-        <div className="px-[5%] h-[50%] pt-3 md:w-2/4 md:h-full xl:w-2/4 flex flex-col justify-between">
+        <div className="px-[5%] h-[50%] pt-3 md:w-2/4 md:h-full flex flex-col justify-between">
           <p className="py-3 md:py-6">
             Summary
           </p>
